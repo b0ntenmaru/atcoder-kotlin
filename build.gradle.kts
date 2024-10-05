338dfee172abab440b046ad539743ccc8aed2df8
Hiroaki Date <mmst3l4@gmail.com>
2024-10-05 16:29:49 +0900

    kotestを導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.20"
+    id("io.kotest") version "0.4.10"
 }
 
 group = "org.example"
@@ -11,11 +12,21 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.9.1")
+    testImplementation("io.kotest:kotest-framework-engine-jvm:5.9.1")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
 kotlin {
     jvmToolchain(18)
 }
+
+apply(plugin = "io.kotest")
